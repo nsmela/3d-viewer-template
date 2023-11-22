c25d5184a8bcfd0d70d75a187a19cf4105d5e26a
nsmela <nsmela@hotmail.com>
2023-11-22 08:56:19 -0800

    Initial structure

---
+++ src/classes/info.py
@@ -0,0 +1,9 @@
+"""
+@file 
+@brief This file contains the global path settings
+@author Nathan Smela <nathan.smela@bccancer.bc.ca>
+"""
+
+import os
+
+PATH = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))  # src folder location
