9c5c62e3bbbce3ab166cf386d78f228aac3ae585
nsmela <nsmela@hotmail.com>
2023-11-23 12:09:33 -0800

    page navigation working

--- src/windows/main_window.py
+++ src/windows/main_window.py
@@ -3,7 +3,7 @@ import os
 
 from classes import info
 from classes.app import get_app
-from .ui import main_window_ui
+from windows.ui import main_window_ui
 
 from PySide6.QtCore import Signal, QFile, QIODevice
 from PySide6.QtWidgets import QMainWindow
@@ -12,16 +12,40 @@ from PySide6.QtUiTools import QUiLoader
 class MainWindow(QMainWindow):
 
     # signals
-    importFileSignal = Signal(str) 
-    clearLoadedModelSignal = Signal()
-    displayUpdatedSignal = Signal()
-    exportFileSignal = Signal()  # need to export the mesh
+    importFileSignal = Signal(str)  
+    clearLoadedModelSignal = Signal()  # when the viewport's contents are cleared
+    displayUpdatedSignal = Signal()  # when the 3d viewport is updated
+    exportFileSignal = Signal(str)  # need to export the mesh
+    viewChangedSignal = Signal(int)  # set the page for the viewwidget
 
     def __init__(self, *args):
         super().__init__(*args)
 
+        self.initialized = False
+
+        # TODO load user settings
+
         self.ui = main_window_ui.Ui_MainWindow()
         self.ui.setupUi(self)
+
+        # TODO set keyboard shortcuts
+        
+        # TODO set theme
+        # TODO set window variables (name, title, position in monitor) 
+
+        # TODO connect signals
+        # view signals send the new page's index
+        self.ui.btn_import_view.pressed.connect(lambda: self.viewChangedSignal.emit(0))
+        self.ui.btn_dicom_view.pressed.connect(lambda: self.viewChangedSignal.emit(1))
+        self.ui.btn_export_view.pressed.connect(lambda: self.viewChangedSignal.emit(2))
+
+        self.viewChangedSignal.connect(self.ui.viewswidget.setCurrentIndex)
+
+        # TODO initialize models
+
+        # show this window with resizing to ensure canvas is displayed properly
+        self.showWithCanvas()  # shows and then resizes the window to properly display canvas
+        self.initialized = True
 
     def showWithCanvas(self):
         # for the canvas widget to properly fit, we need to shrink the window slightly and then 
--- src/windows/ui/main_window_ui.py
+++ src/windows/ui/main_window_ui.py
@@ -15,8 +15,9 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QFont, QFontDatabase, QGradient, QIcon,
     QImage, QKeySequence, QLinearGradient, QPainter,
     QPalette, QPixmap, QRadialGradient, QTransform)
-from PySide6.QtWidgets import (QApplication, QHBoxLayout, QLabel, QMainWindow,
-    QPushButton, QSizePolicy, QVBoxLayout, QWidget)
+from PySide6.QtWidgets import (QApplication, QGraphicsView, QGridLayout, QHBoxLayout,
+    QLabel, QMainWindow, QPushButton, QSizePolicy,
+    QSpacerItem, QStackedWidget, QVBoxLayout, QWidget)
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
@@ -26,60 +27,123 @@ class Ui_MainWindow(object):
         self.centralwidget = QWidget(MainWindow)
         self.centralwidget.setObjectName(u"centralwidget")
         self.verticalLayout = QVBoxLayout(self.centralwidget)
+        self.verticalLayout.setSpacing(0)
         self.verticalLayout.setObjectName(u"verticalLayout")
+        self.verticalLayout.setContentsMargins(0, 0, 0, 0)
         self.top_menu_bar = QWidget(self.centralwidget)
         self.top_menu_bar.setObjectName(u"top_menu_bar")
         self.top_menu_bar.setMinimumSize(QSize(0, 64))
         self.top_menu_bar.setMaximumSize(QSize(16777215, 64))
         self.horizontalLayout = QHBoxLayout(self.top_menu_bar)
+        self.horizontalLayout.setSpacing(1)
         self.horizontalLayout.setObjectName(u"horizontalLayout")
+        self.horizontalLayout.setContentsMargins(0, 0, 0, 0)
+        self.horizontalSpacer = QSpacerItem(40, 20, QSizePolicy.Expanding, QSizePolicy.Minimum)
+
+        self.horizontalLayout.addItem(self.horizontalSpacer)
+
         self.btn_import_view = QPushButton(self.top_menu_bar)
         self.btn_import_view.setObjectName(u"btn_import_view")
+        self.btn_import_view.setMinimumSize(QSize(80, 0))
+        self.btn_import_view.setMaximumSize(QSize(80, 16777215))
 
         self.horizontalLayout.addWidget(self.btn_import_view)
 
+        self.btn_dicom_view = QPushButton(self.top_menu_bar)
+        self.btn_dicom_view.setObjectName(u"btn_dicom_view")
+
+        self.horizontalLayout.addWidget(self.btn_dicom_view)
+
         self.btn_export_view = QPushButton(self.top_menu_bar)
         self.btn_export_view.setObjectName(u"btn_export_view")
+        self.btn_export_view.setMinimumSize(QSize(80, 0))
+        self.btn_export_view.setMaximumSize(QSize(80, 16777215))
 
         self.horizontalLayout.addWidget(self.btn_export_view)
+
+        self.horizontalSpacer_2 = QSpacerItem(40, 20, QSizePolicy.Expanding, QSizePolicy.Minimum)
+
+        self.horizontalLayout.addItem(self.horizontalSpacer_2)
 
 
         self.verticalLayout.addWidget(self.top_menu_bar)
 
-        self.widget_2 = QWidget(self.centralwidget)
-        self.widget_2.setObjectName(u"widget_2")
-        self.horizontalLayout_2 = QHBoxLayout(self.widget_2)
+        self.bodywidget = QWidget(self.centralwidget)
+        self.bodywidget.setObjectName(u"bodywidget")
+        self.horizontalLayout_2 = QHBoxLayout(self.bodywidget)
+        self.horizontalLayout_2.setSpacing(0)
         self.horizontalLayout_2.setObjectName(u"horizontalLayout_2")
-        self.left_menu_bar = QWidget(self.widget_2)
+        self.horizontalLayout_2.setContentsMargins(0, 0, 0, 0)
+        self.left_menu_bar = QWidget(self.bodywidget)
         self.left_menu_bar.setObjectName(u"left_menu_bar")
         self.left_menu_bar.setMinimumSize(QSize(240, 0))
         self.left_menu_bar.setMaximumSize(QSize(240, 16777215))
-        self.label = QLabel(self.left_menu_bar)
+        self.verticalLayout_2 = QVBoxLayout(self.left_menu_bar)
+        self.verticalLayout_2.setObjectName(u"verticalLayout_2")
+        self.viewswidget = QStackedWidget(self.left_menu_bar)
+        self.viewswidget.setObjectName(u"viewswidget")
+        self.page_1 = QWidget()
+        self.page_1.setObjectName(u"page_1")
+        self.label = QLabel(self.page_1)
         self.label.setObjectName(u"label")
-        self.label.setGeometry(QRect(-70, 210, 749, 277))
+        self.label.setGeometry(QRect(80, 60, 47, 13))
+        self.viewswidget.addWidget(self.page_1)
+        self.page_2 = QWidget()
+        self.page_2.setObjectName(u"page_2")
+        self.label_2 = QLabel(self.page_2)
+        self.label_2.setObjectName(u"label_2")
+        self.label_2.setGeometry(QRect(80, 40, 47, 13))
+        self.viewswidget.addWidget(self.page_2)
+        self.page_3 = QWidget()
+        self.page_3.setObjectName(u"page_3")
+        self.label_3 = QLabel(self.page_3)
+        self.label_3.setObjectName(u"label_3")
+        self.label_3.setGeometry(QRect(50, 90, 47, 13))
+        self.viewswidget.addWidget(self.page_3)
+
+        self.verticalLayout_2.addWidget(self.viewswidget)
+
+        self.verticalSpacer = QSpacerItem(20, 296, QSizePolicy.Minimum, QSizePolicy.Expanding)
+
+        self.verticalLayout_2.addItem(self.verticalSpacer)
+
 
         self.horizontalLayout_2.addWidget(self.left_menu_bar)
 
-        self.display_widget = QWidget(self.widget_2)
-        self.display_widget.setObjectName(u"display_widget")
-        self.display_widget.setMinimumSize(QSize(600, 540))
+        self.displayviewwidget = QWidget(self.bodywidget)
+        self.displayviewwidget.setObjectName(u"displayviewwidget")
+        self.displayviewwidget.setMinimumSize(QSize(600, 540))
+        self.gridLayout = QGridLayout(self.displayviewwidget)
+        self.gridLayout.setObjectName(u"gridLayout")
+        self.gridLayout.setContentsMargins(0, 0, 0, 0)
+        self.graphicsView = QGraphicsView(self.displayviewwidget)
+        self.graphicsView.setObjectName(u"graphicsView")
 
-        self.horizontalLayout_2.addWidget(self.display_widget)
+        self.gridLayout.addWidget(self.graphicsView, 0, 0, 1, 1)
 
 
-        self.verticalLayout.addWidget(self.widget_2)
+        self.horizontalLayout_2.addWidget(self.displayviewwidget)
+
+
+        self.verticalLayout.addWidget(self.bodywidget)
 
         MainWindow.setCentralWidget(self.centralwidget)
 
         self.retranslateUi(MainWindow)
+
+        self.viewswidget.setCurrentIndex(2)
+
 
         QMetaObject.connectSlotsByName(MainWindow)
     # setupUi
 
     def retranslateUi(self, MainWindow):
         MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"MainWindow", None))
-        self.btn_import_view.setText(QCoreApplication.translate("MainWindow", u"PushButton", None))
-        self.btn_export_view.setText(QCoreApplication.translate("MainWindow", u"PushButton", None))
-        self.label.setText(QCoreApplication.translate("MainWindow", u"EXAMPLE", None))
+        self.btn_import_view.setText(QCoreApplication.translate("MainWindow", u"import mesh", None))
+        self.btn_dicom_view.setText(QCoreApplication.translate("MainWindow", u"import dicom", None))
+        self.btn_export_view.setText(QCoreApplication.translate("MainWindow", u"export mesh", None))
+        self.label.setText(QCoreApplication.translate("MainWindow", u"mesh", None))
+        self.label_2.setText(QCoreApplication.translate("MainWindow", u"dicom", None))
+        self.label_3.setText(QCoreApplication.translate("MainWindow", u"export", None))
     # retranslateUi
 
